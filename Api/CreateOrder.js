889e86211ffdef1115ef6d8dda4fe55367cfc39c
hikmh <hikmanrl8@gmail.com>
2023-11-05 14:46:51 +0700

    Update ke4

--- Api/CreateOrder.js
+++ Api/CreateOrder.js
@@ -11,11 +11,11 @@ export const options = {
 };
 
 export default function (){
-    group('Add to Cart', () => {
-        Addtocart();
+    group('Create Order', () => {
+        CreateOrder();
     })
 }
-function Addtocart() {
+function CreateOrder() {
     // const getaccesstoken = Login()
     const accessToken = ''
     const headers = {
@@ -23,9 +23,36 @@ function Addtocart() {
         'content-type': 'application/json',
     };
     const apiUrl = 'https://staging-superapp-api.superapp.co.id/api/v5/app/order/create';
-    const payload = JSON.stringify({});
+    const payload = JSON.stringify({
+        "customer_id":19261,
+        "address_id":18989,
+        "voucher_id":null,
+        "voucher_type":null,
+        "delivery_date":"2023-11-01",
+        "discount":0,
+        "estimation_time":"07:00 - 9:00",
+        "grand_total":200000,
+        "coin_used":0,
+        "items":[{
+            "cart_id":1004,
+            "city":"kota-surabaya",
+            "product_id":2703,
+            "id":27700,
+            "product_attribute_id":4417,
+            "qty":2,
+            "selling_price":100000,
+            "discount":0,"final_price":200000
+        }],
+        "method":"COD",
+        "note":"",
+        "payment_type":"COD",
+        "sub_total":200000,
+        "is_jawara":false,
+        "quick_order":false,
+        "potential_cashback":0
+    });
     const response = http.post(apiUrl,payload, {headers});
     check(response, {
-        'Success Add to Cart': (r) => r.status === 201
+        'Success Create Order': (r) => r.status === 201
     });
 }
